f4f2e4ca931d45f31e06d8c6fc0460fb5e8f4b87
Andrii Tataurov <andtat@qamadness.com>
2026-06-03 18:07:20 +0300

    Add lesson17 tasks

---
+++ lesson17/pom/pages/BasePage.ts
@@ -0,0 +1,26 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export default class BasePage {
+    page: Page;
+    url: string;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.url = '';
+    }
+
+    async navigateTo() {
+        await this.page.goto(this.url);
+    }
+
+    async validateEmptyErrorMessage(locator: Locator) {
+        await expect(locator).toHaveJSProperty('validity.valueMissing', true)
+        const message = await locator.evaluate(el => (el as HTMLInputElement).validationMessage);
+        expect(message).toMatch(/Please fill (in|out) this field\./);
+    }
+
+    async validateIncorrectErrorMessage(locator: Locator) {
+        const message = await locator.textContent();
+        expect(message).toMatch('Username or password is incorrect');
+    }
+}
